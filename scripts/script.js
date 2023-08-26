da3a384656ec9d91933575084338a779596a0d57
Adrian <adriangarcia8382@gmail.com>
2023-08-26 18:32:41 +0800

    Finish the Project

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+const favicon = document.getElementById("favicon");
 const img = document.querySelector("img");
 
 img.addEventListener("click", switchTheme);
@@ -5,9 +6,11 @@ img.addEventListener("click", switchTheme);
 function switchTheme() {
   document.body.classList.toggle("dark-mode");
 
-  if (!document.body.classList.contains("dark-mode")) {
+  if (document.body.classList.contains("dark-mode")) {
+    favicon.setAttribute("href", "images/waning-moon.png");
     img.src = "images/sun.png";
   } else {
+    favicon.setAttribute("href", "images/sun.png");
     img.src = "images/waning-moon.png";
   }
 }
